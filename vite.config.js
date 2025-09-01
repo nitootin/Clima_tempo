e53ac50b53478ef1417fdf56c0b7bc4668a52ac6
nitootin <phillip.profissional@gmail.com>
2025-09-01 20:14:56 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  //base: '/Clima_tempo/', 
+  base: '/Clima_tempo', 
   plugins: [react()],
 })
